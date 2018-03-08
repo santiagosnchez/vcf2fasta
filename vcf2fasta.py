c86e8de1030a3d4a3e9eaf1bc6d67df97445453b
Santiago Sanchez-Ramirez <santiago.snchez@gmail.com>
2018-03-07 22:40:05 -0500

    major updates

--- vcf2fasta.py
+++ vcf2fasta.py
@@ -4,7 +4,9 @@
 import argparse
 import gzip
 import sys
-from re import match
+import os
+from re import match, search, sub
+from string import maketrans
 
 def main():
     # parse arguments
@@ -31,27 +33,184 @@ python vcf2fasta.py -f genome.fas -v variants.vcf -g regions.gff -f CDS --blend
     '--gff', '-g', metavar='GFF', type=str, required=True,
     help='individual FASTA records.')
     a4 = parser.add_argument(
-    '--feat', '-e', nargs="*", metavar='FEAT', type=str,
+    '--feat', '-e', nargs="*", metavar='FEAT', type=str, required=True,
     help='individual FASTA records.')
     parser.add_argument(
     '--blend', '-b', action="store_true", default=False,
     help='if sequences should be printed to screen.')
     args = parser.parse_args()
+    # read genome
     genome = readfasta(args.fasta)
+    # read gff
     gff = readgff(args.gff)
-    print "\nlines in GFF file: %s" % len(gff)
-    if args.feat == None:
-        feat = list(set([ gff[i][2] for i in range(len(gff)) ]))
-        featstr = raw_input("Which feature(s) would you like to extract?\n"+
-            " ".join(feat)+"\n"+
-            "Type them separated by a space if multiple:")
-        feat = featstr.split(" ")
+    print "Lines in GFF file: %s" % len(gff)
+    # get features
+    feat = args.feat
+    # parse vcf file
+    if search(".vcf.gz$", args.vcf):
+        v = gzip.open(args.vcf, "r")
     else:
-        feat = args.feat
-    print feat[0]
+        v = open(args.vcf, "r")
+    for var in v:
+        if match("##", var):
+            next
+        elif match("#CHROM", var):
+            head = var.rstrip().split("\t")
+            sampstart = head.index('FORMAT')+1
+            samples = head[sampstart:]
+            data = {}
+            count = 0
+        else:
+            var = var.rstrip().split("\t")
+            data = vcf2fasta(var, gff, genome, head, sampstart, feat[0], data)
+            count += 1
+            sys.stdout.write("Reading SNP: %s\r" % count)
+    # save data to disk
+    genes = data.keys()
+    print ""
+    for fe in feat:
+        if not os.path.exists(fe):
+            os.makedirs(fe)
+        for gene in genes:
+            if args.blend:
+                with open(fe+"/"+gene) as o:
+                    for indiv in samples:
+                        o.write(">"+indiv+"\n")
+                        for region in sorted([ int(i) for i in data[gene][fe].keys() ]):
+                            strand = data[gene][fe][str(region)].keys()[0]
+                            seq += data[gene][fe][str(region)][strand][indiv]
+                        if strand == '-':
+                            seq = revcomp(seq)
+                        o.write(seq+"\n")
+                    sys.stdout.write("Writing: "+fe+"/"+gene+"\r")
+            else:
+                for region in sorted([ int(i) for i in data[gene][fe].keys() ]):
+                    strand = data[gene][fe][str(region)].keys()[0]
+                    with open(fe+"/"+gene+"."+region) as o:
+                        for indiv in samples:
+                            o.write(">"+indiv+"\n")
+                            seq = data[gene][fe][str(region)][strand][indiv]
+                            if strand == '-':
+                                seq = revcomp(seq)
+                            o.write(seq+"\n")
+                    sys.stdout.write("Writing: "+fe+"/"+gene+"."+region+"\r")
+    print "Done"
+
+
+
+    
+
+
+    
 
 
 # functions
+
+def vcf2fasta(var, gff, genome, head, sampstart, feat, data):
+    start = [ gff[var[0]][i][3] for i in range(len(gff[var[0]])) ]
+    end = [ gff[var[0]][i][4] for i in range(len(gff[var[0]])) ]
+    features = [ gff[var[0]][i][2] for i in range(len(gff[var[0]])) ]
+    strand = [ gff[var[0]][i][6] for i in range(len(gff[var[0]])) ]
+    gname = [ gff[var[0]][i][8] for i in range(len(gff[var[0]])) ]
+    for i in zip(features,start,end,strand,gname):
+        if i[0] == feat:
+            if int(i[1]) <= int(var[1]) <= int(i[2]):
+                gname = getgnames(i[-1])
+                data = startdict(data,gname)
+                data[gname] = startdict(data[gname],feat)
+                data[gname][feat] = startdict(data[gname],i[1])
+                data[gname][feat][i[1]] = startdict(data[gname],i[3])
+                for j in range(sampstart,len(head)):
+                    if checkphase(var, j) == 'phased':
+                        pheada = head[j]+"_a"
+                        pheadb = head[j]+"_b"
+                        if keyisfound(data[gname][feat][i[1]][i[3]],pheada):
+                            data = updatealn(data, gname, feat, i[1], pheada, var, j, i[3])
+                        elif keyisfound(data[gname][feat][i[1]][i[3]],pheadb):
+                            data = updatealn(data, gname, feat, i[1], pheadb, var, j, i[3])
+                        else:
+                            data[gname][feat][i[1]][i[3]][pheada] = genome[var[0]][int(i[1])-1:int(i[2])]
+                            data[gname][feat][i[1]][i[3]][pheadb] = genome[var[0]][int(i[1])-1:int(i[2])]
+                            data = updatealn(data, gname, feat, i[1], pheada, var, j, i[3])
+                            data = updatealn(data, gname, feat, i[1], pheadb, var, j, i[3])
+                    else:
+                        if keyisfound(data[gname][feat][i[1]][i[3]],head[j]):
+                            data = updatealn(data, gname, feat, i[1], head[j], var, j, i[3])
+                        else:
+                            data[gname][feat][i[1]][i[3]][head[j]] = genome[var[0]][int(i[1])-1:int(i[2])]
+                            data = updatealn(data, gname, feat, i[1], head[j], var, j, i[3])
+    return data
+
+def updatealn(data, gname, feat, start, samp, var, ind, strand):
+    seq = data[gname][feat][start][strand][samp].upper()
+    pos = int(var[1])-int(start)
+    alleles = {'0':var[3], '.':'?'}
+    j = 0
+    if search(',', var[4]):
+        alt = var[4].split(',')
+        for i in alt:
+            j += 1
+            alleles[str(j)] = i
+    else:
+        alleles['1'] = var[4]
+    gt = var[ind].split(":")[0]
+    if checkphase(var, ind) == 'phased':
+        nvar = [ alleles[i] for i in gt.split("|") ]
+        if search("_a$", samp):
+            data[gname][feat][start][strand][samp] = seq[:pos] + nvar[0] + seq[pos+1:]
+        elif search("_b$", samp):
+            data[gname][feat][start][strand][samp] = seq[:pos] + nvar[1] + seq[pos+1:]
+        return data
+    elif checkphase(var, ind) == 'unphased':
+        nvar = [ alleles[i] for i in gt.split("/") ]
+        if all([ i == nvar[0] for i in nvar ]):
+            data[gname][feat][start][strand][samp] = seq[:pos] + nvar[0] + seq[pos+1:]
+        else:
+            data[gname][feat][start][strand][samp] = seq[:pos] + iupac(nvar) + seq[pos+1:]
+        return data
+    elif checkphase(var, ind) == 'haploid':
+        data[gname][feat][start][strand][samp] = seq[:pos] + alleles[gt] + seq[samp][pos+1:]
+        return data
+
+def checkphase(var, ind):
+    gt = var[ind].split(":")[0]
+    if search('|', gt):
+        return 'phased'
+    elif search('/', gt):
+        return 'unphased'
+    else:
+        return 'haploid'
+
+def iupac(nvar):
+    myiupac = {
+    'AT':'W','TA':'W',
+    'AC':'M','CA':'M',
+    'AG':'R','GA':'R',
+    'TC':'Y','CT':'Y',
+    'TG':'K','GT':'K',
+    'GC':'S','CG':'S',
+    }
+    return myiupac[''.join(nvar)]
+
+def revcomp(seq):
+    tt = maketrans('ACGT?N','TGCA?N')
+    return seq[::-1].translate(tt)
+
+def getgnames(gname):
+    m1 = search('\"(.+?)\" *;{0,1}', gname)
+    m2 = search('= *\"(.+?)\" *;{0,1}', gname)
+    m3 = search('= *(.+?) *;{0,1}', gname)
+    m4 = search('^(.+?) *;{0,1}$', gname)
+    if m1:
+        name = sub('\"| *;*$','',m1.group())
+    elif m2:
+        name = sub('= *|\"| *;*$','',m2.group())
+    elif m3:
+        name = sub('= *| *;*$','',m3.group())
+    elif m4:
+        name = sub(' *;*$','',m4.group())
+    return name
+
 
 def readfasta(file):
     data = {}
@@ -66,6 +225,7 @@ def readfasta(file):
                 sys.stdout.write("FASTA seqs read: %s\r" % seqn),
             else:
                 data[head] += line
+        print ""
         return data
 
 def readgff(file):
@@ -76,7 +236,7 @@ def readgff(file):
                 next
             else:
                 line = line.rstrip().split("\t")
-                if ifkeyisfound(gff,line[0]):
+                if keyisfound(gff,line[0]):
                     gff[line[0]].append(line)
                 else:
                     gff[line[0]] = []
@@ -91,7 +251,14 @@ def wrapseq(seq, w):
             chunks.append(seq[i:interval[interval.index(i)+1]-1])
     return("\n".join(chunks))
 
-def ifkeyisfound(x, key):
+def startdict(d, y):
+    if keyisfound(d, y):
+        return d
+    else:
+        d[y] = {}
+        return startdict(d, y)
+
+def keyisfound(x, key):
     try:
         x[key]
         return True
